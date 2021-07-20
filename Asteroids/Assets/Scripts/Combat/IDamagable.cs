20b344fc456d4171907d84596ad3abaccaccef33
Zakhar Pustovgarov <deadspace1999@mail.ru>
2021-07-20 09:37:03 +0300

    Скрипты были удалены и заменены на библиотеку
    Также были изменены префабы

--- Asteroids/Assets/Scripts/Combat/IDamagable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Интерфейс для объектов, которым может наноситься урон
-public interface IDamagable
-{
-    public void TakeDamage(string damageType);
-}
